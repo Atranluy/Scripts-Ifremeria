c21dab6fd02c2f969c64917b002e4597da26d3d3
Adrien Tran Lu Y <84977797+Atranluy@users.noreply.github.com>
2021-09-27 15:20:58 +0200

    Update Multi_Launch_dadi.sh

--- Dadi_script/Multi_Launch_dadi.sh
+++ Dadi_script/Multi_Launch_dadi.sh
@@ -4,8 +4,9 @@
 #$ -V
 
 ##### script from Adrien Tran Lu Y
-######### change header depending of type of HPC you will use (grid cluster engine or slurm)
-######### im using pyenv environnement where dadi is installed but you can call the dadi version from module load or something else depending on your HPC
+######### change header depending of type of HPC you will use (SGE or slurm protocol)
+######### im using pyenv (python environnement) environnement where dadi is installed but you can call the dadi version from module load or something else depending on your HPC
+######### my pyenv is called "dadi_sim"
 ######### dadi v2.1 from https://gitlab.mbb.univ-montp2.fr/khalid/dadi/-/tree/master  see there to install dadi and pyenv to install it within a specific python3 environnement
 
 
@@ -13,8 +14,8 @@ FS=Spectrum_FS_nosingleton ###### Spectrum file
 
 
 #### set name of each model do you want to launch 
-#### it will create a single bash file for each run (1 to 11) for each model and run it independently 
-#### it check before if the iteration is already done within the folder (current folder /IM/IM_1.txt) for exemple. 
+#### it will create a single bash file for each run (1 to 11) for each model and running it independently 
+#### it check before if the iteration is already done within the folder (current folder /IM/IM_1.txt for exemple) and if it's already done it wont start the simulation. 
 for model in SC2N2m AM2N2m;  
 do
 
