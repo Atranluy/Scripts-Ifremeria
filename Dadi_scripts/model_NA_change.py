573b06683ece85bbcf1ca7f18033132012a7489f
Adrien Tran Lu Y <84977797+Atranluy@users.noreply.github.com>
2023-03-07 10:45:16 +0100

    Corr Ts and Tam in the AM model
    Small correction made on the simplest AM model (AM) where Ts and Tam have been inverted. 
    No impact on the AIC and paramters value, as Ts and Tam are estimated with the same bounds and independently.

--- Dadi_scripts/model_NA_change.py
+++ Dadi_scripts/model_NA_change.py
@@ -75,7 +75,6 @@ def IMG(params, ns, pts):
     nu1, nu2,nuA,b1, b2,Ta, Ts, m12, m21= params
 
     xx = dadi.Numerics.default_grid(pts)
-
     phi = dadi.PhiManip.phi_1D(xx)
     phi = dadi.Integration.one_pop(phi, xx,Ta, nu = nuA)
     phi = dadi.PhiManip.phi_1D_to_2D(xx, phi)
@@ -638,9 +637,9 @@ def AM(params, ns, pts):
     phi = dadi.Integration.one_pop(phi, xx,Ta, nu = nuA)
 
     phi = dadi.PhiManip.phi_1D_to_2D(xx, phi)
-    phi = dadi.Integration.two_pops(phi, xx, Ts, nu1, nu2,
-                               m12=m12, m21=m21)
     phi = dadi.Integration.two_pops(phi, xx, Tam, nu1, nu2,
+                               m12=m12, m21=m21)
+    phi = dadi.Integration.two_pops(phi, xx, Ts, nu1, nu2,
                                m12=0, m21=0)
     fs = dadi.Spectrum.from_phi(phi, ns, (xx,xx))
     return fs
@@ -669,9 +668,9 @@ def AMa(params, ns, pts):
     phi = dadi.PhiManip.phi_1D_to_2D(xx, phi)
     nu1_func = lambda t: s * (nu1/s)**(t/T)
     nu2_func = lambda t: (1-s) * (nu2/(1-s))**(t/T)
-    phi = dadi.Integration.two_pops(phi, xx, Ts, nu1_func, nu2_func,
-                               m12=m12, m21=m21)
     phi = dadi.Integration.two_pops(phi, xx, Tam, nu1_func, nu2_func,
+                               m12=m12, m21=m21)
+    phi = dadi.Integration.two_pops(phi, xx, Ts, nu1_func, nu2_func,
                                m12=0, m21=0)
     fs = dadi.Spectrum.from_phi(phi, ns, (xx,xx))
     return fs
